abba8c448c0339b8fe9f5378ff6297383800bf79
cjmamo <823038+cjmamo@users.noreply.github.com>
2024-10-03 09:14:58 +0200

    refactor: turn route Java DSL into YAML

--- src/main/resources/archetype-resources/src/test/java/route/OpenHimMediatorRouteTestCase.java
+++ src/main/resources/archetype-resources/src/test/java/route/OpenHimMediatorRouteTestCase.java
@@ -6,7 +6,7 @@ package ${package}.route;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.camel.builder.AdviceWith;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.model.ToDynamicDefinition;
+import org.apache.camel.model.ToDefinition;
 import org.apache.camel.test.spring.junit5.UseAdviceWith;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @UseAdviceWith
-public class OpenHimMediatorRouteBuilderTestCase extends AbstractRouteFunctionalTestCase {
+public class OpenHimMediatorRouteTestCase extends AbstractRouteFunctionalTestCase {
 
     @Value("classpath:openhim/mediator-config.ds")
     private Resource mediatorConfigResource;
@@ -33,7 +33,7 @@ public class OpenHimMediatorRouteBuilderTestCase extends AbstractRouteFunctional
         AdviceWith.adviceWith(
                 camelContext,
                 "openHimRegisterRoute",
-                a -> a.weaveByType(ToDynamicDefinition.class).replace().to("mock:register"));
+                a -> a.weaveByType(ToDefinition.class).replace().to("mock:register"));
 
         MockEndpoint endpoint = camelContext.getEndpoint("mock:register", MockEndpoint.class);
         endpoint.setExpectedCount(1);
@@ -56,7 +56,7 @@ public class OpenHimMediatorRouteBuilderTestCase extends AbstractRouteFunctional
         AdviceWith.adviceWith(
                 camelContext,
                 "openHimHeartbeatRoute",
-                a -> a.weaveByType(ToDynamicDefinition.class).replace().to("mock:heartbeat"));
+                a -> a.weaveByType(ToDefinition.class).replace().to("mock:heartbeat"));
 
         MockEndpoint endpoint = camelContext.getEndpoint("mock:heartbeat", MockEndpoint.class);
         endpoint.setExpectedCount(1);
